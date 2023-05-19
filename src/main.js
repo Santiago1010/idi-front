46bacfac4dbe32760786b48451cd3a178118159c
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2023-05-19 12:40:22 -0500

    [Feature] Agregado soporte multilingüe.
    Se ha agregado soporte multilingüe al proyecto. Se importó la biblioteca 'vue-i18n' y se creó una instancia de 'createI18n' en el archivo 'main.js'. Se obtuvo el código de país del navegador del usuario y se utilizó como idioma inicial en la configuración de 'createI18n'. Se estableció el idioma de respaldo como español ('es') en caso de que no se encuentre una traducción específica. Se importó el archivo de traducción 'es.json' ubicado en la carpeta 'utils/locales'.
    
    En el archivo 'App.vue', se agregó el complemento de 'i18n' mediante el uso del método '.use(i18n)'.
    
    Además, se creó el archivo 'es.json' en la ubicación 'utils/locales' con la traducción en español para la clave 'welcome', que se establece como 'Bienvenido'. Esta traducción se utilizará en el proyecto para proporcionar un mensaje de bienvenida en español.
    
    Estos cambios permitirán al proyecto adaptarse a diferentes idiomas y mejorar la experiencia de los usuarios internacionales.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/es'
+import { createI18n } from 'vue-i18n'
 
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
@@ -18,6 +19,16 @@ import 'quasar/src/css/index.sass'
 // Import general styles
 import './style.css'
 
+var countryCode = navigator.language.split('-')[0];
+
+const i18n = createI18n({
+  locale: countryCode, // Establece el idioma inicial
+  fallbackLocale: 'es', // Establece el idioma de respaldo si no se encuentra la traducción
+  messages: {
+    es: require('./utils/locales/es.json'),
+  },
+})
+
 import App from './App.vue'
 
 createApp(App)
@@ -25,4 +36,5 @@ createApp(App)
   plugins: {}, // import Quasar plugins and add here
   lang: quasarLang,
 })
+.use(i18n) // Agrega el complemento de VueI18n
 .mount('#app')
