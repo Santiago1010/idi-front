60c1bcd3169ed28a588dbec2f76f01bc61b2f564
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2023-05-19 10:58:41 -0500

    Agregar dependencias de Quasar y estilos personalizado
    Este commit realiza los siguientes cambios:
    
    1. Se agregaron las dependencias de Quasar y se actualizó la versión de Quasar a la 2.12.0.
    2. Se añadió el plugin '@quasar/extras' en su versión 1.16.4.
    3. Se agregó el plugin '@quasar/vite-plugin' en su versión 1.3.3.
    4. Se actualizó la versión de axios a '^1.4.0', de pinia a '^2.1.3', de vue a '^3.2.47', de vue-i18n a '^8.28.2' y de vue-router a '^4.2.1'.
    5. En el archivo 'src/App.vue', se movió la declaración del componente HelloWorld al final del archivo, después del template, y se agregó la declaración 'script setup' antes del template.
    6. En el archivo 'src/App.vue', se agregaron estilos personalizados para la clase '.logo'.
    7. Se añadió el archivo 'src/assets/styles/sass/quasar-variables.sass' que contiene variables de estilo para Quasar.
    8. En el archivo 'src/main.js', se importaron los módulos necesarios de Quasar y se configuró su uso, incluyendo el idioma español.
    9. Se importaron bibliotecas de iconos y animaciones de Quasar.
    10. Se importaron los estilos generales del proyecto.
    11. En el archivo 'vite.config.js', se añadieron los plugins de Vue y Quasar, y se configuró la ruta del archivo de variables de estilo de Quasar.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/es'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+
+// A few examples for animations from Animate.css:
+import '@quasar/extras/animate/fadeIn.css'
+import '@quasar/extras/animate/fadeOut.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+// Import general styles
 import './style.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+})
+.mount('#app')
