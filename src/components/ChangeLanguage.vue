ccebb4cab2c042de375877ea29ae0ec45350166a
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2023-05-20 09:00:09 -0500

    [Feature] Añadido cambio de idioma en el menú desplegable.
    Se ha añadido un nuevo cambio de idioma en el menú desplegable del componente ChangeLanguage.vue. Ahora, en lugar de usar un select con opciones, se ha implementado un menú desplegable personalizado con estilos CSS. El menú desplegable muestra una caja de texto con el texto del idioma actual seleccionado y al hacer clic en ella se despliegan las opciones de idioma disponibles. Cada opción de idioma muestra una bandera y el nombre del idioma. Al seleccionar una opción, se cambia el idioma de la aplicación. Además, se ha añadido un efecto visual en forma de flecha que rota cuando el menú está desplegado. Estos cambios permiten una experiencia de cambio de idioma más intuitiva y agradable para el usuario.

--- src/components/ChangeLanguage.vue
+++ src/components/ChangeLanguage.vue
@@ -1,13 +1,19 @@
 <template>
-  <select v-model="$i18n.locale" label="Cambiar idioma">
-    <option :value="$t('changeLanguage')" default>{{ $t('changeLanguage') }}</option>
+  <!--<select v-model="$i18n.locale" label="Cambiar idioma">
     <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-  </select>
+  </select>-->
+  <div class="dropdown" :class="{ active: dropdownActive }" @click="dropdownActive = !dropdownActive">
+    <input type="text" class="textBox" :placeholder="$t('changeLanguage')" readonly>
+
+    <div class="options">
+      <div v-for="language in optionsLanguages" :key="language.language" @click="$i18n.locale = language.locale"><span :class="`fi fi-${language.class}`"></span> {{ language.language }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
   // Importar internos de vue
-  import { ref } from 'vue'
+  import { ref, onMounted } from 'vue'
 
   // Importar store
   import { useLanguageStore } from '../stores/LanguageStore.js'
@@ -19,41 +25,111 @@
     {
       locale: 'de',
       language: 'Deutsch',
-      class: ''
+      class: 'de'
     },
     {
       locale: 'en',
       language: 'English',
-      class: ''
+      class: 'us'
     },
     {
       locale: 'es',
       language: 'Español',
-      class: ''
+      class: 'co'
     },
     {
       locale: 'fr',
       language: 'Français',
-      class: ''
+      class: 'fr'
     },
     {
       locale: 'it',
       language: 'Italiano',
-      class: ''
+      class: 'it'
     },
     {
       locale: 'pt',
       language: 'Português',
-      class: ''
+      class: 'pt'
     }
   ]);
-
-  // Funciones y métodos
-  const changelanguage = (newLanguage) => {
-    localStorage.removeItem('language');
-
-    if (!localStorage.language) {
-      languageStore.setNewLanguage({ newLanguage })
-    }
-  }
+  const dropdownActive = ref(false)
 </script>
+
+<style scoped>
+  .dropdown {
+    position: relative;
+    margin: 1% 0;
+    width: 300px;
+    height: 50px;
+  }
+
+  .dropdown::before {
+    content: '';
+    position: absolute;
+    right: 20px;
+    top: 18px;
+    z-index: 1000;
+    width: 8px;
+    height: 8px;
+    border: 2px solid black;
+    border-top: 2px solid white;
+    border-right: 2px solid white;
+    transform: rotate(-45deg);
+    transition: 0.5s;
+    pointer-events: none;
+  }
+
+  .dropdown input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    background: #FFFFFF;
+    border: none;
+    outline: none;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.05);
+    padding: 12px 20px;
+    border-radius: 10px;
+    text-transform: uppercase;
+  }
+
+  .dropdown.active::before {
+    top: 22px;
+    transform: rotate(-225deg);
+  }
+
+  .dropdown .options {
+    position: absolute;
+    top: 70px;
+    width: 50%;
+    margin-left: 25%;
+    background: #FFFFFF;
+    box-shadow: 0 30px 30px rgba(0, 0, 0, 0.05);
+    border-radius: 10px;
+    overflow: hidden;
+    color: #333333;
+    display: none;
+  }
+
+  .dropdown.active .options {
+    display: block;
+  }
+
+  .dropdown .options div {
+    padding: 12px 20px;
+    cursor: pointer;
+  }
+
+  .dropdown .options div:hover {
+    background-color: #D3D3D3;
+  }
+
+  .dropdown .options div span.fi {
+    position: relative;
+    top: 1px;
+    font-size: 1.2em;
+  }
+</style>
